c1ac2ae322a384e97d9a1c4170efc7f60795a6d5
Anthony Warrington <Anthony.Warrington@gallagher.com>
2026-05-04 14:53:04 +1200

    commit changes before session

--- test/TicTacToeTest/BoardStateTests.cs
+++ test/TicTacToeTest/BoardStateTests.cs
@@ -6,5 +6,68 @@ namespace TicTacToeTest;
 [TestClass]
 public sealed class BoardStateTests
 {
-    
+    [TestMethod]
+    [DataRow(3, 3, 0, 0)]
+    [DataRow(3, 3, 1, 1)]
+    [DataRow(4, 4, 2, 2)]
+    [DataRow(5, 2, 4, 1)]
+    public void When_position_is_in_range_and_has_no_marker__returns_null(int width, int height, int posX, int posY)
+    {
+        // Arrange
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
+        BoardState boardState = gameBoard.CurrentState;
+
+        // Act
+        var actual = boardState[position];
+
+        // Assert
+        Assert.IsNull(actual);
+    }
+
+    [TestMethod]
+    [DataRow(3, 3, 0, 0, Marker.X)]
+    [DataRow(3, 3, 1, 1, Marker.O)]
+    [DataRow(4, 4, 3, 3, Marker.X)]
+    [DataRow(5, 2, 4, 1, Marker.O)]
+    public void When_position_is_in_range_and_has_marker__returns_correct_marker(int width, int height, int posX, int posY, Marker marker)
+    {
+        // Arrange
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
+        _ = gameBoard.TryPlaceMarker(marker, position);
+        BoardState boardState = gameBoard.CurrentState;
+        var expected = marker;
+
+        // Act
+        var actual = boardState[position];
+
+        // Assert
+        Assert.IsNotNull(actual);
+        Assert.AreEqual(expected, actual.Value);
+    }
+
+    [TestMethod]
+    [DataRow(3, 3, -1, 0)]
+    [DataRow(3, 3, 0, -1)]
+    [DataRow(3, 3, 3, 0)]
+    [DataRow(4, 4, 0, 4)]
+    [DataRow(5, 2, 5, 0)]
+    public void When_position_is_out_of_range__returns_null(int width, int height, int posX, int posY)
+    {
+        // Arrange
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
+        BoardState boardState = gameBoard.CurrentState;
+
+        // Act
+        var actual = boardState[position];
+
+        // Assert
+        Assert.IsNull(actual);
+    }
+
+    // TODO: "When board state is snapshot does not reflect subsequent changes to board"?
+    // i.e. when BoardState captures the board, any subsequent changes madeto the GameBoard aren't visible
+    // through it
 }
--- test/TicTacToeTest/GameBoardTests.cs
+++ test/TicTacToeTest/GameBoardTests.cs
@@ -57,33 +57,34 @@ public sealed class GameBoardTests
     // <------------------------- TryPlaceMarker ------------------------->
 
     // This could be four tests - Marker.X < 0, X > BoardSize.X, same for Y
-    // replace 3x3 method with actual
     [TestMethod]
-    public void TryPlaceMarker__when_position_is_out_of_range__returns_false()
+    [DataRow(3, 3, 4, 4, Marker.X)]
+    [DataRow(1, 1, 1, 1, Marker.O)]
+    [DataRow(2, 5, 2, 5, Marker.X)]
+    public void TryPlaceMarker__when_position_is_out_of_range__returns_false(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
-        Position position = new() { X = 4, Y = 4};
-        GameBoard gameBoard = Create3x3Board();
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
         bool expected = false;
 
         // Act
-        var actual = gameBoard.TryPlaceMarker(Marker.X, position);
+        var actual = gameBoard.TryPlaceMarker(marker, position);
 
         // Assert
         Assert.AreEqual(expected, actual);
     }
 
     [TestMethod]
-    [DataRow(0, 1, Marker.O)]
-    [DataRow(1, 0, Marker.O)]
-    [DataRow(2, 1, Marker.X)]
-    [DataRow(0, 2, Marker.X)]
-    public void TryPlaceMarker__when_space_is_empty_and_position_is_in_range__returns_true(int posX, int posY, Marker marker)
+    [DataRow(3, 3, 0, 1, Marker.O)]
+    [DataRow(3, 3, 1, 0, Marker.O)]
+    [DataRow(4, 4, 3, 3, Marker.X)]
+    [DataRow(5, 2, 4, 1, Marker.X)]
+    public void TryPlaceMarker__when_space_is_empty_and_position_is_in_range__returns_true(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
-        Position position = new() { X = posX, Y = posY};
-        GameBoard gameBoard = Create3x3Board();
-
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
         var expected = true;
 
         // Act
@@ -94,57 +95,56 @@ public sealed class GameBoardTests
     }
 
     [TestMethod]
-    [DataRow(0, 1, Marker.O)]
-    [DataRow(1, 0, Marker.O)]
-    [DataRow(2, 1, Marker.X)]
-    [DataRow(0, 2, Marker.X)]
-    public void TryPlaceMarker__when_space_is_empty_and_position_is_in_range__space_is_set_to_marker(int posX, int posY, Marker marker)
+    [DataRow(3, 3, 0, 1, Marker.O)]
+    [DataRow(3, 3, 1, 0, Marker.O)]
+    [DataRow(4, 4, 3, 3, Marker.X)]
+    [DataRow(5, 2, 4, 1, Marker.X)]
+    public void TryPlaceMarker__when_space_is_empty_and_position_is_in_range__space_is_set_to_marker(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
-        Position position = new() { X = posX, Y = posY};
-        GameBoard gameBoard = Create3x3Board();
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
         var expected = marker;
 
         // Act
-        _ = gameBoard.TryPlaceMarker(marker, position);        
+        _ = gameBoard.TryPlaceMarker(marker, position);
         var actual = gameBoard.GetMarkerAtPosition(position);
-        
+
         // Assert
         Assert.IsNotNull(actual);
         Assert.AreEqual(expected, actual.Value);
     }
 
     [TestMethod]
-    [DataRow(0, 1, Marker.O)]
-    [DataRow(1, 0, Marker.O)]
-    [DataRow(2, 1, Marker.X)]
-    [DataRow(0, 2, Marker.X)]
-    public void TryPlaceMarker__when_space_is_not_empty_and_position_is_in_range__returns_false(int posX, int posY, Marker marker)
+    [DataRow(3, 3, 0, 1, Marker.O)]
+    [DataRow(3, 3, 1, 0, Marker.O)]
+    [DataRow(4, 4, 3, 3, Marker.X)]
+    [DataRow(5, 2, 4, 1, Marker.X)]
+    public void TryPlaceMarker__when_space_is_not_empty_and_position_is_in_range__returns_false(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
-        Position position = new() { X = posX, Y = posY};
-        GameBoard gameBoard = Create3x3Board();
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
         var expected = false;
         _ = gameBoard.TryPlaceMarker(marker, position);
 
         // Act
         var actual = gameBoard.TryPlaceMarker(marker, position);
-        
+
         // Assert
         Assert.AreEqual(expected, actual);
     }
 
-    // try to place marker when space is not empty, original value in that space doesn't change
     [TestMethod]
-    [DataRow(0, 1, Marker.O, Marker.X)]
-    [DataRow(1, 0, Marker.O, Marker.X)]
-    [DataRow(2, 1, Marker.X, Marker.O)]
-    [DataRow(0, 2, Marker.X, Marker.O)]
-    public void TryPlaceMarker__when_space_is_not_empty_and_position_is_in_range__original_marker_in_space_does_not_change(int posX, int posY, Marker originalMarker, Marker newMarker)
+    [DataRow(3, 3, 0, 1, Marker.O, Marker.X)]
+    [DataRow(3, 3, 1, 0, Marker.O, Marker.X)]
+    [DataRow(4, 4, 3, 3, Marker.X, Marker.O)]
+    [DataRow(5, 2, 4, 1, Marker.X, Marker.O)]
+    public void TryPlaceMarker__when_space_is_not_empty_and_position_is_in_range__original_marker_in_space_does_not_change(int width, int height, int posX, int posY, Marker originalMarker, Marker newMarker)
     {
         // Arrange
-        Position position = new() { X = posX, Y = posY};
-        GameBoard gameBoard = Create3x3Board();
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
         _ = gameBoard.TryPlaceMarker(originalMarker, position);
         var expected = originalMarker;
 
@@ -157,15 +157,15 @@ public sealed class GameBoardTests
     }
 
     [TestMethod]
-    [DataRow(-1, 0, Marker.X)]
-    [DataRow(0, -1, Marker.O)]
-    [DataRow(3, 0, Marker.X)]
-    [DataRow(0, 3, Marker.O)]
-    public void TryPlaceMarker__when_position_is_out_of_range_on_valid_board__returns_false(int posX, int posY, Marker marker)
+    [DataRow(3, 3, -1, 0, Marker.X)]
+    [DataRow(3, 3, 0, -1, Marker.O)]
+    [DataRow(4, 4, 4, 0, Marker.X)]
+    [DataRow(5, 2, 0, 2, Marker.O)]
+    public void TryPlaceMarker__when_position_is_out_of_range_on_valid_board__returns_false(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
         Position position = new() { X = posX, Y = posY };
-        GameBoard gameBoard = Create3x3Board();
+        GameBoard gameBoard = new(width, height);
         bool expected = false;
 
         // Act
@@ -175,64 +175,44 @@ public sealed class GameBoardTests
         Assert.AreEqual(expected, actual);
     }
 
-    // Not sure about this test - there's no reason it would happen with a dictionary, but if the
-    // implementation ever changes from a dictionary, I feel like its still something that needs to be covered
     [TestMethod]
-    [DataRow(-1, 0, Marker.X)]
-    [DataRow(0, -1, Marker.O)]
-    [DataRow(3, 0, Marker.X)]
-    [DataRow(0, 3, Marker.O)]
-    public void TryPlaceMarker__when_position_is_out_of_range__adjacent_valid_position_remains_null(int posX, int posY, Marker marker)
+    [DataRow(3, 3, 0, 0, Marker.X)]
+    [DataRow(3, 3, 1, 1, Marker.O)]
+    [DataRow(4, 4, 1, 0, Marker.X)]
+    public void TryPlaceMarker__when_marker_is_placed__every_other_position_remains_unchanged(
+        int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
-        Position outOfBounds = new() { X = posX, Y = posY };
-        Position adjacentValid = new() { X = 0, Y = 0 };
-        GameBoard gameBoard = Create3x3Board();
+        Position position = new() { X = posX, Y = posY };
+        GameBoard gameBoard = new(width, height);
 
         // Act
-        _ = gameBoard.TryPlaceMarker(marker, outOfBounds);
-        var actual = gameBoard.GetMarkerAtPosition(adjacentValid);
+        _ = gameBoard.TryPlaceMarker(marker, position);
 
         // Assert
-        Assert.IsNull(actual);
-    }
-
-    [TestMethod]
-    [DataRow(0, 0, Marker.X, 1, 1, Marker.O)]
-    [DataRow(0, 0, Marker.O, 2, 2, Marker.X)]
-    [DataRow(1, 0, Marker.X, 0, 1, Marker.O)]
-    // TODO: when a marker is placed, none of the markers at other positions change
-    public void TryPlaceMarker__when_two_markers_placed_at_different_positions__each_position_holds_correct_marker(
-        int posX1, int posY1, Marker marker1, int posX2, int posY2, Marker marker2)
-    {
-        // Arrange
-        Position position1 = new() { X = posX1, Y = posY1 };
-        Position position2 = new() { X = posX2, Y = posY2 };
-        GameBoard gameBoard = Create3x3Board();
-
-        // Act
-        _ = gameBoard.TryPlaceMarker(marker1, position1);
-        _ = gameBoard.TryPlaceMarker(marker2, position2);
-        var actual1 = gameBoard.GetMarkerAtPosition(position1);
-        var actual2 = gameBoard.GetMarkerAtPosition(position2);
-
-        // Assert
-        Assert.AreEqual(marker1, actual1);
-        Assert.AreEqual(marker2, actual2);
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Position otherPosition = new() { X = x, Y = y };
+                if (otherPosition == position) continue;
+                Assert.IsNull(gameBoard.GetMarkerAtPosition(otherPosition));
+            }
+        }
     }
 
     // <------------------------- GetMarkerAtPosition ------------------------->
 
     [TestMethod]
-    [DataRow(0, 1)]
-    [DataRow(1, 0)]
-    [DataRow(2, 1)]
-    [DataRow(0, 2)]
-    public void GetMarkerAtPosition__when_position_has_no_marker__returns_null(int posX, int posY)
+    [DataRow(3, 3, 0, 1)]
+    [DataRow(3, 3, 1, 0)]
+    [DataRow(4, 4, 3, 3)]
+    [DataRow(5, 2, 4, 1)]
+    public void GetMarkerAtPosition__when_position_has_no_marker__returns_null(int width, int height, int posX, int posY)
     {
         // Arrange
         Position position = new() { X = posX, Y = posY };
-        GameBoard gameBoard = Create3x3Board();
+        GameBoard gameBoard = new(width, height);
 
         // Act
         var actual = gameBoard.GetMarkerAtPosition(position);
@@ -242,15 +222,15 @@ public sealed class GameBoardTests
     }
 
     [TestMethod]
-    [DataRow(0, 1, Marker.O)]
-    [DataRow(1, 0, Marker.O)]
-    [DataRow(2, 1, Marker.X)]
-    [DataRow(0, 2, Marker.X)]
-    public void GetMarkerAtPosition__when_position_has_marker__returns_marker(int posX, int posY, Marker marker)
+    [DataRow(3, 3, 0, 1, Marker.O)]
+    [DataRow(3, 3, 1, 0, Marker.O)]
+    [DataRow(4, 4, 3, 3, Marker.X)]
+    [DataRow(5, 2, 4, 1, Marker.X)]
+    public void GetMarkerAtPosition__when_position_has_marker__returns_marker(int width, int height, int posX, int posY, Marker marker)
     {
         // Arrange
         Position position = new() { X = posX, Y = posY };
-        GameBoard gameBoard = Create3x3Board();
+        GameBoard gameBoard = new(width, height);
         _ = gameBoard.TryPlaceMarker(marker, position);
         var expected = marker;
 
@@ -263,18 +243,18 @@ public sealed class GameBoardTests
     }
 
     [TestMethod]
-    [DataRow(-1, 0)]
-    [DataRow(0, -1)]
-    [DataRow(3, 3)]
-    [DataRow(99, 99)]
-    public void GetMarkerAtPosition__when_position_is_out_of_range__throws_ArgumentOutOfRangeException(int posX, int posY)
+    [DataRow(3, 3, -1, 0)]
+    [DataRow(3, 3, 0, -1)]
+    [DataRow(3, 3, 3, 3)]
+    [DataRow(4, 4, 99, 99)]
+    public void GetMarkerAtPosition__when_position_is_out_of_range__throws_ArgumentOutOfRangeException(int width, int height, int posX, int posY)
     {
         // Arrange
         Position position = new() { X = posX, Y = posY };
-        GameBoard gameBoard = Create3x3Board();
+        GameBoard gameBoard = new(width, height);
 
         // Act
-        void act() {gameBoard.GetMarkerAtPosition(position);}
+        void act() { gameBoard.GetMarkerAtPosition(position); }
 
         // Assert
         Assert.Throws<ArgumentOutOfRangeException>(act);
